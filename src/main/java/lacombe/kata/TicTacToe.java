76af0bf4036e66ba1619fd33551e2c9c42f0f805
YoannWicker <yoann.wicker@lacombedulionvert.fr>
2022-02-24 16:47:43 +0100

    fix error : is_over_when_all_fields_are_taken_and_one_player_wins

--- src/main/java/lacombe/kata/TicTacToe.java
+++ src/main/java/lacombe/kata/TicTacToe.java
@@ -19,14 +19,12 @@ public class TicTacToe {
     }
 
     public void play(Player player, int x, int y) throws Exception {
-        assert !state.equals(IS_OVER);
         if(List.of(IS_OVER, PLAYER_O_WINS, PLAYER_X_WINS).contains(state)) {
             throw new GameStateIsOverException();
         }
         if(!player.equals(state.getPlayer())) {
             throw new PlayerTurnException(player);
         }
-        assert player.equals(state.getPlayer());
 
         grid.set(player, new Coordinate(x, y));
 
@@ -34,10 +32,10 @@ public class TicTacToe {
     }
 
     private GameState changeState(Player player) {
-        if(grid.allFieldsAreTaken()) {
-            return IS_OVER;
-        } else if(fieldSequences.anyIsCompletelyTokenBy(player)){
+        if(fieldSequences.anyIsCompletelyTokenBy(player)){
             return state.gameWonBy(player);
+        } else if(grid.allFieldsAreTaken()) {
+            return IS_OVER;
         }
         return state.defaultNextState();
     }
